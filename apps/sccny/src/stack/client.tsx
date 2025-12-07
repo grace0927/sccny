bcc1d70740fd8e1bd0a64de8ebfed93c5bef7b20
Jianyu Feng <cellphone2008@gmail.com>
2025-12-06 19:33:28 -0500

    fix: sign up

--- apps/sccny/src/stack/client.tsx
+++ apps/sccny/src/stack/client.tsx
@@ -2,7 +2,6 @@ import { StackClientApp } from "@stackframe/stack";
 
 export const stackClientApp = new StackClientApp({
   tokenStore: "nextjs-cookie",
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
   projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
   publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
 });
